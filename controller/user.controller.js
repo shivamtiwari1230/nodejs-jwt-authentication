5002bd935fe5aedd132cb9e75ad62d830bd04795
shivamtiwari1230 <shivamtiwari1234550@gmail.com>
2025-03-15 21:59:47 +0530

    controllers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,62 @@
+import { log } from "console";
+import User from "../model/User.model.js";
+import crypto from "crypto"
+
 const registerUser = async (req, res) => {
-    res.send("registered");
+    // get data 
+    // check if user already exists
+    // create a user in database
+    // create a verification token
+    // save token in database
+    // send token as email to user
+    // send success status to user
+
+    const {name, email, password} = req.body
+    if(!name || !email || !password){
+        return res.status(400).json({
+            message: "All fields are required"
+        });
+    }
+    // check validation
+
+try {
+    const existingUser = await User.findOne({email})
+    if(existingUser){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
+
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+
+    if(!user){
+        return res.status(400).json({
+            message: "User not registered",
+        })
+    }
+
+ const token = crypto.randomBytes(32).toString("hex")
+ console.log(token);
+ user.verificationToken = token
+
+ await user.save()
+
+
+
+
+
+
+} catch (error) {
+    
+}
+    
 }
 
 export {registerUser}
+
--- model/user.model.js
+++ model/user.model.js
@@ -1,28 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: {
-    type: String,
-    enum: ["user", "admin"],
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
   },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: {
-    type: String,
-  },
-  resetPasswordToken: {
-    type: String,
-  },
-  resetPasswordExpires: {
-    type: Date,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
